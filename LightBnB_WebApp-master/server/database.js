02d5dbd3d680c2ed94031a87291c10e56495c4af
HoHoHoang <hoangdinh90@gmail.com>
2020-02-06 23:50:03 +0000

    addProperty functionality

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -156,7 +156,7 @@ const getAllProperties = function(options, limit = 10) {
   `;
 
   // 5
-  console.log(options, queryString, queryParams);
+  // console.log(options, queryString, queryParams);
 
   // 6
   return pool.query(queryString, queryParams)
@@ -171,9 +171,22 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const propertyArray = [property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, property.country, property.street, property.city, property.province, property.post_code];
+
+  const query =
+  `INSERT INTO properties (title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
+  RETURNING *;`;
+
+  console.log(property, 'this is just the property stuff')
+
+  return pool.query(query, propertyArray)
+  .then(res => res.rows)
+  .catch(err => err)
+
+  // const propertyId = Object.keys(properties).length + 1;
+  // property.id = propertyId;
+  // properties[propertyId] = property;
+  // return Promise.resolve(property);
 }
 exports.addProperty = addProperty;
